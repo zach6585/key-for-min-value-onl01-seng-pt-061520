7564112acaed9c4ad22b4a80836b75b4277b83a7
zachary mines <zmines@wesleyan.edu>
2020-06-24 19:16:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  i = ""
-  j = ""
+  i = nil
+  j = nil
   name_hash.each do |k,v|
     if i == ""
       i = v 
